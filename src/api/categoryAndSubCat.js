03e3436e850c7fe5a67bd4cd6cc1894bac314f8b
MohamedxOU <oucharroumohemed@gmail.com>
2025-09-09 19:49:30 +0100

    feat: Add CRUD operations for categories and subcategories in categoryAndSubCat API

--- src/api/categoryAndSubCat.js
+++ src/api/categoryAndSubCat.js
@@ -13,10 +13,133 @@ export const getCategories = async () => {
   }
 }
 
+//get category with it's subcategories by id
+export const getCategoryWithSubCategories = async (id) => {
+  try {
+    const response = await apiClient.get(`/categories/${id}`, {
+    });
+    return response.data;
+  }
+  catch (error) {
+    throw error.response ? error.response.data : error.message;
+  }
+}
+
+
+
+//create category
+export const createCategory = async (categoryData, token) => {
+  try {
+    const response = await apiClient.post("/categories", categoryData, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return response.data;
+  }
+  catch (error) {
+    throw error.response ? error.response.data : error.message;
+  }
+}
+
+//update category
+export const updateCategory = async (id, categoryData, token) => {
+  try {
+    const response = await apiClient.put(`/categories/${id}`, categoryData, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return response.data;
+  }
+  catch (error) {
+    throw error.response ? error.response.data : error.message;
+  }
+}
+
+//delete category
+export const deleteCategory = async (id, token) => {
+  try {
+    const response = await apiClient.delete(`/categories/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+
+    });
+    return response.data;
+  }
+
+  catch (error) {
+    throw error.response ? error.response.data : error.message;
+  }
+}
+
+
+
+
 //get all subcategories
 export const getSubCategories = async () => {
   try {
     const response = await apiClient.get("/subcategories", {
+    });
+    return response.data;
+  }
+  catch (error) {
+    throw error.response ? error.response.data : error.message;
+  }
+}
+
+//get subcategory by id
+export const getSubCategoryById = async (id) => {
+  try {
+    const response = await apiClient.get(`/subcategories/${id}`, {
+    });
+    return response.data;
+  }
+  catch (error) {
+    throw error.response ? error.response.data : error.message;
+  }
+}
+
+//create subcategory
+export const createSubCategory = async (subCategoryData, token) => {
+  try {
+    const response = await apiClient.post("/subcategories", subCategoryData, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return response.data;
+
+  }
+  catch (error) {
+    throw error.response ? error.response.data : error.message;
+  }
+}
+
+//update subcategory
+export const updateSubCategory = async (id, subCategoryData, token) => {
+  try {
+    const response = await apiClient.put(`/subcategories/${id}`, subCategoryData, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return response.data;
+  }
+  catch (error) {
+    throw error.response ? error.response.data : error.message;
+  }
+}
+
+//delete subcategory
+export const deleteSubCategory = async (id, token) => {
+  try {
+    const response = await apiClient.delete(`/subcategories/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+
     });
     return response.data;
   }
